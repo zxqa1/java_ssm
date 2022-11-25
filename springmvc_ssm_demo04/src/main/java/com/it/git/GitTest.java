203413173a053146749d000c198df94c9bec8a6e
zxqdgitee <zxqdyx2021@163.com>
2022-11-25 19:53:52 +0800

    second commit test local update

--- springmvc_ssm_demo04/src/main/java/com/it/git/GitTest.java
+++ springmvc_ssm_demo04/src/main/java/com/it/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello 0 master");
         System.out.println("hello git 0");
+        System.out.println("git local update");
+
     }
 }
