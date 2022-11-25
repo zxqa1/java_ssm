f5a5e619a9973202930565f3229b0f7da786cf98
zxqa1 <108708121+zxqa1@users.noreply.github.com>
2022-11-25 20:02:01 +0800

    third commit test remote update

--- springmvc_ssm_demo04/src/main/java/com/it/git/GitTest.java
+++ springmvc_ssm_demo04/src/main/java/com/it/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("git hello 0 master");
         System.out.println("hello git 0");
         System.out.println("git local update");
-
+        System.out.println("git remote update");
     }
 }
